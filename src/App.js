49f1bf38a1ab022fb79de9583903a5257b8c5567
enteecaay <kietnguyen060204@gmail.com>
2024-10-25 00:09:55 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Confirmation from './Confirmation';
+
+function App() {
+    return (
+        <div className="App">
+            <Confirmation />
+        </div>
+    );
+}
+
+export default App;
